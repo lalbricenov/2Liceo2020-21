aa11e8049a7835c401d4f812cd29d762318d3fc9
Luis Briceño <lalbricenov@gmail.com>
2020-11-03 15:35:03 -0500

    While y do while2

---
+++ Clase 8/bmi.js
@@ -0,0 +1,4 @@
+// pedir y validar altura del usuario
+// pedir y validar masa del usuario
+// calcular indice de masa corporal
+// clasificar la persona de acuerdo al valor del indice de masa corporal. Ayuda: usar if, else

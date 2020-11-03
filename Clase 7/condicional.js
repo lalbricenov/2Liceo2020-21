906c4c8e273a08ff2a90f18de7426925a8b06315
Luis Briceño <lalbricenov@gmail.com>
2020-11-03 13:00:24 -0500

    clase 7

---
+++ Clase 7/condicional.js
@@ -0,0 +1,6 @@
+let numero = parseFloat(prompt("Ingrese un numero"));
+
+// b==0  da como resultado true o false
+if (numero == 0) {
+  alert("El numero es cero");
+}
---
+++ Clase 7/operadores.js
@@ -0,0 +1,28 @@
+// numeros
+let x = 4;
+let y = 5;
+// backtick string `
+// console.log("La suma es:" + x + y);
+console.log(`La suma es: ${x + y}`);
+console.log(`La resta es: ${x - y}`);
+console.log(`La multiplicación es: ${x * y}`);
+console.log(`La división es: ${x / y}`);
+console.log(`El módulo es: ${x % y}`);
+
+// cadena de caracteres
+let a = "hola";
+let b = "seconda";
+console.log(`La concatenación es: ${a + " " + b}`);
+console.log(`La resta es ${a - b}`);
+console.log(`La multiplicacion es ${a * b}`);
+console.log(`La división es ${a / b}`);
+
+// variables lógicas
+let p = true;
+let q = false;
+
+console.log(`p: ${p}, q: ${q}`);
+console.log(`Or logico: ${p || q}`);
+console.log(`And logico: ${p && q}`);
+console.log(`No p: ${!p}`);
+console.log(`No q: ${!q}`);

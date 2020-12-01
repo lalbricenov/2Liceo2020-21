682ed0eca77b11e455cf9307db5cecdd7499b042
Luis Briceño <lalbricenov@gmail.com>
2020-12-01 14:06:24 -0500

    Funciones recursivas

---
+++ Clase 12/factorial.js
@@ -0,0 +1,21 @@
+// Version con bucles
+function factorialBucles(n) {
+  let resultado = 1;
+  let factor = 2;
+  while (factor <= n) {
+    resultado = resultado * factor;
+    factor = factor + 1;
+  }
+  return resultado;
+}
+
+function factorial(n) {
+  // Version recursiva de la funcion factorial
+  // Si n es menor o igual a 1 el resultado es inmediato
+  if (n <= 1) return 1;
+  else {
+    // Se debe llamar al factorial del numero anterior
+    console.log(`Calculando ${n} * factorial(${n - 1})`);
+    return n * factorial(n - 1);
+  }
+}

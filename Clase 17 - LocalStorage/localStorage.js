68701a8645a265977a9a841350b89f3c0e8add6b
Luis Briceño <lalbricenov@gmail.com>
2021-02-23 14:34:15 -0500

    Local storage null

--- Clase 17 - LocalStorage/localStorage.js
+++ Clase 17 - LocalStorage/localStorage.js
@@ -22,3 +22,21 @@ localStorage.setItem("notasCorrectas", JSON.stringify(notas));
 let notasCorrectasLeidas = JSON.parse(localStorage.getItem("notasCorrectas"));
 
 console.log(notas[3]);
+
+//Si se trata de leer una variable que no existe en el localStorage
+// como resultado se obtiene null
+ let miVariable = localStorage.getItem("notaz");
+ console.log(miVariable);
+
+ // Si la variable no existe se muestra un mensaje en la consola y 
+ // se crea la variable en el localstorage para que la próxima vez 
+ // que se visite la página sí exista.
+ if(miVariable == null){
+     console.log('La variable notaz no existe en el localStorage')
+     console.log('Ahora voy a crear la variable');
+     localStorage.setItem("notaz", JSON.stringify([1, 3, 4,9, 7,8]))
+ }
+ else{
+     console.log("La variable si existe")
+ }
+

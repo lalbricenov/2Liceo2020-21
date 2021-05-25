68ed82142f560e3cb3810751f406ef16d3879751
Luis Briceño <lalbricenov@gmail.com>
2021-05-25 14:48:45 -0500

    Evento mouse

--- Clase 25 - Evento mouse/eventos.js
+++ Clase 25 - Evento mouse/eventos.js
@@ -3,23 +3,20 @@ let canvas = document.querySelector("#miCanvas")
 // escoger el contexto, en este caso el contexto 2d
 let ctx = canvas.getContext("2d")
 
-function dibujarBoton(xC, yC, r){
-    // dibujo el circulo
+function dibujarPelota(xC, yC, r, color){
+    // console.log(color)
     ctx.beginPath();
-    ctx.strokeStyle = 'blue';
-    ctx.fillStyle = 'blue';
+    ctx.strokeStyle = color
+    ctx.fillStyle = color;
+    // console.log(ctx.strokeStyle)
     ctx.arc(xC, yC, r, 0, 2*Math.PI);
     ctx.stroke();
     ctx.fill();
-    
-    // dibujo el cuadrado
-    ctx.strokeStyle = 'black';
-    ctx.beginPath();
-    ctx.strokeRect(xC-r, yC-r, 2*r, 2*r);
 }
-
 let x = 0;
 let y = 200;
+let r = 30;
+let color = "red";
 
 let vX = 10;// en px/s
 let dT = 30;
@@ -28,7 +25,8 @@ function moverYDibujar(){
     ctx.clearRect(0,0, 600, 400);
     let dX = dT/1000 *vX
     x = x + dX;
-    dibujarBoton(x, y, 10);
+    // console.log(color)
+    dibujarPelota(x, y, r, color);
 }
 
 function arrancarOParar(){
@@ -51,5 +49,16 @@ window.onkeyup = function(event){
         arrancarOParar();
     }
     // cómo identificar cuál fue la tecla presionada?
+}
+
+function cambiarColorPelota(){
+    color = "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+window.onclick = function(event){
+    console.log(`Se hizo click en las coordenadas ${event.offsetX}, ${event.offsetY}`);
+    console.log(`Las coordenadas de la pelota son ${x}, ${y}, y el radio es ${r}`)
+    // Si se hizo click sobre la pelota
+    // Cambiar su color a uno aleatorio.
+    cambiarColorPelota();
 }
 
